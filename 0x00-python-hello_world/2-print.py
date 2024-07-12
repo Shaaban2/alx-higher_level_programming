23e50aea40d18fc625ce3560cf15567d503fbfaa
Shaaban Reda <93987801+Shaaban2@users.noreply.github.com>
2024-07-12 08:50:50 -0700

    Add files via upload

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,5 +1,2 @@
-#!/usr/bin/bash/python3
-"""Prints a specific string."""
-
-print("Programming is like building a multilingual puzzle")
-
+#!/usr/bin/python3
+print("\"Programming is like building a multilingual puzzle")
